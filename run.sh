b0083647544920890f98f43764498afb7e826611
Aatricks <melis.emilio1@gmail.com>
2025-02-02 21:48:58 +0100

    chore: update run.sh to install Python 3.10 and added original script for stable-fast debugging

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 VENV_DIR=.venv
-
-sudo apt-get install python3.12 python3.12-venv python3.12-full
+# for WSL2 Ubuntu install
+# sudo apt install software-properties-common
+# sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get install python3.10 python3.10-venv python3.10-full python3-pip
 
 # Check if .venv exists
 if [ ! -d "$VENV_DIR" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv $VENV_DIR
+    python3.10 -m venv $VENV_DIR
 fi
 
 # Activate the virtual environment
@@ -17,31 +19,13 @@ source $VENV_DIR/bin/activate
 echo "Upgrading pip..."
 pip install --upgrade pip
 
-# Install build dependencies first
-echo "Installing build dependencies..."
-pip install setuptools wheel ninja
+pip3 install --index-url https://download.pytorch.org/whl/cu121 \
+    'torch==2.2.2' torchvision 'xformers>=0.0.22' 'triton>=2.1.0' \
+    'stable_fast-1.0.5+torch222cu121-cp310-cp310-manylinux2014_x86_64.whl'
 
-# Check for CUDA installation
-if nvidia-smi &> /dev/null; then
-    echo "CUDA detected, installing PyTorch with CUDA support..."
-    pip install xformers torch torchvision --index-url https://download.pytorch.org/whl/cu124
-
-    # Install stable-fast without CUDA
-    echo "Installing stable-fast without CUDA support..."
-    # Create a temporary script to set environment variable and run pip
-    cat > temp_install.sh << 'EOF'
-#!/bin/bash
-export WITH_CUDA=0
-pip install -v -U git+https://github.com/chengzeyi/stable-fast.git@main#egg=stable-fast
-EOF
-
-    chmod +x temp_install.sh
-    ./temp_install.sh
-    rm temp_install.sh
-fi
 # Install tkinter
 echo "Installing tkinter..."
-sudo apt-get install python3.12-tk
+sudo apt-get install python3.10-tk
 
 # Install additional requirements
 if [ -f requirements.txt ]; then
@@ -53,7 +37,8 @@ fi
 
 # Launch the script
 echo "Launching LightDiffusion..."
-python3.12 ./modules/user/GUI.py
+# python3.10 ./modules/user/GUI.py
+python3.10 LightDiffusion.py
 
 # Deactivate the virtual environment
 deactivate
