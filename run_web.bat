1c0dd1beea96f1b05e1ae8b6e05c7e8760ca7019
Aatricks <melis.emilio1@gmail.com>
2025-02-10 23:10:54 +0100

    fix: minor fix on batch file leading to pytorch compiling without cuda

--- run_web.bat
+++ run_web.bat
@@ -18,17 +18,13 @@ REM Install specific packages
 echo Installing required packages...
 pip install uv
 
-REM Check GPU type
-SET TORCH_URL=https://download.pytorch.org/whl/cpu
-nvidia-smi >nul 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    echo NVIDIA GPU detected
-    SET TORCH_URL=https://download.pytorch.org/whl/cu124
-    uv pip install xformers torch torchvision --index-url %TORCH_URL%
-) ELSE (
-    echo No compatible GPU detected, using CPU
-    uv pip install torch torchvision --index-url %TORCH_URL%
-)
+REM Check for NVIDIA GPU
+SET TORCH_URL=https://download.pytorch.org/whl/cu124
+uv pip install xformers torch torchvision --index-url %TORCH_URL%
+
+REM For CPU
+REM SET TORCH_URL=https://download.pytorch.org/whl/cpu
+REM uv pip install torch torchvision --index-url %TORCH_URL%
 
 REM Install additional requirements
 IF EXIST requirements.txt (
